a45451b0661f000390f3a224d1aa1adbf6cf0da7
liummingqiao <50901715+liummingqiao@users.noreply.github.com>
2019-09-02 14:20:12 +0800

    添加功能

---
+++ web/src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="px-3 py-2 bg-black d-flax ai-center">
+      <img src="../assets/king.jpg" height="30" />
+      <div class="px-2">
+        <div class="text-white ">王者荣耀</div>
+        <div class="text-hui fs-xxs">团队成就更多</div>
+      </div>
+      <button class="bg-primary">立即下载</button>
+    </div>
+
+    <div class="nve">nve</div>
+
+    <router-view></router-view>
+  </div>
+</template>
