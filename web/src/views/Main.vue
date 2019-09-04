7f693f1674548c9ee73e02b3cabc9b8ec3925627
liummingqiao <50901715+liummingqiao@users.noreply.github.com>
2019-09-04 08:15:33 +0800

    King

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="px-3 py-2 bg-black d-flax ai-center">
       <img src="../assets/king.jpg" height="30" />
+
       <div class="px-2 flex-grow-1" >
         <div class="text-white ">王者荣耀</div>
         <div class="text-hui fs-xxs">团队成就更多</div>
